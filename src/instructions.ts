cb667caa27b27efcd77139405ddbbba3c7a5126b
Rômulo Zoch <romuloctba@gmail.com>
2023-05-28 19:55:10 -0300

    make instructions better

--- src/instructions.ts
+++ src/instructions.ts
@@ -10,10 +10,10 @@ export function showInstructions() {
     </div>
     
     <ol>
-        <li>Envie o Mene para adicionar o selo.</li>
-        <li>Escolha entre os selos disponíveis, ou envie o seu próprio selo.</li>
-        <li>Posicione, escolha o tamanho, e altere a transparência.</li>
-        <li>Faça download do seu Mene finalizado<span class="news-badge">**</span></li>
+        <li>Envie o seu Mene</li>
+        <li>Escolha um selo, ou envie</li>
+        <li>Posicione</li>
+        <li>Faça download!<span class="news-badge">**</span></li>
 
         <li class="warning">
         **Atenção: Nem todos os watermenes são compliance com a regras do grupo. Contacte o SAC/Moderação em caso de dúvida.
