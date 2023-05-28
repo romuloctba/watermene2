ce5b79dfb1849980e97ac6d4ac9e7831d578a702
Rômulo Zoch <romuloctba@gmail.com>
2023-05-28 13:02:08 -0300

    update instructions

--- src/instructions.ts
+++ src/instructions.ts
@@ -11,12 +11,10 @@ export function showInstructions() {
     
     <ol>
         <li>Envie o Mene para adicionar o selo.</li>
-        <li>Escolha entre os selos disponíveis<span class="news-badge">*</span> ou envie o seu próprio selo.</li>
+        <li>Escolha entre os selos disponíveis, ou envie o seu próprio selo.</li>
         <li>Posicione, escolha o tamanho, e altere a transparência.</li>
         <li>Faça download do seu Mene finalizado<span class="news-badge">**</span></li>
-        <li class="warning">
-            *Em breve
-        </li>
+
         <li class="warning">
         **Atenção: Nem todos os watermenes são compliance com a regras do grupo. Contacte o SAC/Moderação em caso de dúvida.
         </li>
