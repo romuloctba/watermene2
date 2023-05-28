4d816c09023ebdf6bc7034de3fc11a47b7865c67
Rômulo Zoch <romuloctba@gmail.com>
2023-05-28 11:05:31 -0300

    use promise in slider

--- src/seals-slider.ts
+++ src/seals-slider.ts
@@ -23,42 +23,45 @@ async function getSeals() {
       counter++;
     })
   }
+
+  return Promise.resolve();
 }
 
-await getSeals();
+getSeals().then(() => {
+  for (let i = 0; i < seals.length; i++) {
+    console.log('creating el ', i);
+    const sealControl = document.createElement("button");
+    sealControl.setAttribute(DATA_ATTRIBUTE, seals[i]);
+    sealControl.classList.add("slide");
+    sealControl.addEventListener('click', function() {
+      const chosenImg = this.getElementsByTagName('img')[0];
+      chosenSeal(chosenImg);
+    });
+  
+  
+    const loadColumn = (elements: IntersectionObserverEntry[]) => {
+      console.log('intercepted')
+  
+      setTimeout(() => {
+        elements.forEach((element) => {
+          if (element.isIntersecting) {
+            const currentCounter = element.target.getAttribute(DATA_ATTRIBUTE);
+            const img = document.createElement('img');
+            img.setAttribute('src', '/seals/seal_' + currentCounter + '.png');
+            element.target.appendChild(img);
+  
+            observer.unobserve(element.target);
+          }
+        });
+      }, 750);
+    };
+  
+    const observer = new IntersectionObserver(loadColumn);
+    const target = sealControl;
+    observer.observe(target);
+  
+    container.appendChild(sealControl);
+  }
+});
 
-for (let i = 0; i < seals.length; i++) {
-  console.log('creating el ', i);
-  const sealControl = document.createElement("button");
-  sealControl.setAttribute(DATA_ATTRIBUTE, seals[i]);
-  sealControl.classList.add("slide");
-  sealControl.addEventListener('click', function() {
-    const chosenImg = this.getElementsByTagName('img')[0];
-    chosenSeal(chosenImg);
-  });
-
-
-  const loadColumn = (elements: IntersectionObserverEntry[]) => {
-    console.log('intercepted')
-
-    setTimeout(() => {
-      elements.forEach((element) => {
-        if (element.isIntersecting) {
-          const currentCounter = element.target.getAttribute(DATA_ATTRIBUTE);
-          const img = document.createElement('img');
-          img.setAttribute('src', '/seals/seal_' + currentCounter + '.png');
-          element.target.appendChild(img);
-
-          observer.unobserve(element.target);
-        }
-      });
-    }, 750);
-  };
-
-  const observer = new IntersectionObserver(loadColumn);
-  const target = sealControl;
-  observer.observe(target);
-
-  container.appendChild(sealControl);
-}
 
